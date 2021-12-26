e8a35641e0f26c09d4a52ec8d1629d7e4a4ba5f6
raihara3 <raihara3@gmail.com>
2021-12-26 12:08:06 +0900

    add: create editor.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,20 @@
 import * as React from "react"
 import { render } from "react-dom"
-import styled from "styled-components"
+import { createGlobalStyle } from "styled-components"
 
-const Header = styled.h1`
-  color: blue;
+import Editor from "./pages/editor"
+
+const GlobalStyle = createGlobalStyle`
+  body * {
+    box-sizing: border-box;
+  }
 `
 
-const Main = (<Header>Markdown Editor</Header>)
+const Main = (
+  <>
+    <GlobalStyle />
+    <Editor />
+  </>
+)
 
 render(Main, document.getElementById("app"))
